c3aa314cdb19e2d738d68220a83a8619e9a2e834
Sebastian Döll <sebastian@katallaxie.me>
2023-07-16 07:40:05 +0000

    wip: parse tmpl

--- pkg/tmpl/tmpl.go
+++ pkg/tmpl/tmpl.go
@@ -1,1 +1,23 @@
 package tmpl
+
+import (
+	"io"
+	"strings"
+	"text/template"
+)
+
+// Parse ...
+func Parse(src io.Reader, dst io.Writer, data interface{}) error {
+	buf := new(strings.Builder)
+	_, err := io.Copy(buf, src)
+	if err != nil {
+		return err
+	}
+
+	tmpl, err := template.New("tmpl").Funcs(tmplFuncs).Parse(buf.String())
+	if err != nil {
+		return err
+	}
+
+	return tmpl.Execute(dst, data)
+}
